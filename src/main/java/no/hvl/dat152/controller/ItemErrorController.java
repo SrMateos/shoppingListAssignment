b3775fca59ba2e31d008add9fe6118d5acbdb961
SrMateos <xavimateosm@gmail.com>
2022-11-10 18:37:37 +0100

    First commit

---
+++ src/main/java/no/hvl/dat152/controller/ItemErrorController.java
@@ -0,0 +1,18 @@
+package no.hvl.dat152.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class ItemErrorController implements ErrorController {
+
+    @RequestMapping(path = "/error")
+    public String handleError(RedirectAttributes redirectAttrs){
+
+        String msg = "An error occurred. Please try again.";
+        redirectAttrs.addFlashAttribute("errormsg", msg);
+        return "redirects:/viewitems";
+    }
+}
